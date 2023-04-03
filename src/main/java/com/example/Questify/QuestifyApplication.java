36f9d8dfa19d0a9a29ce72c575a6f61d19d04288
mihaelageobumbar <bumbarmihaelageorgiana@gmail.com>
2023-04-04 00:05:27 +0300

    Add existing project files to Git

---
+++ src/main/java/com/example/Questify/QuestifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Questify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Questify/QuestifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Questify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
